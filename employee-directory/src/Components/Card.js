f6895467b045e0a9d21588c5baed95913a5b4d7c
Mary Jacobs <TheMaryJacobs@gmail.com>
2020-01-28 11:35:21 -0600

    arrange card layout

--- employee-directory/src/Components/Card.js
+++ employee-directory/src/Components/Card.js
@@ -11,12 +11,43 @@ function Card({ employee }) {
           {employee.firstName} {employee.lastName}
         </h1>
         <div className="data">
-        <h3><small>Role | </small>{employee.role}</h3>
+        <div className="dataRow">
+          <small className="dataType">Role | </small>
+          <h3 className="dataCol"> {employee.role}</h3>
+        </div>
+        <br/>
+        <div className="dataRow">
+          <small className="dataType">Pronouns | </small>
+          <h3 className="dataCol"> {employee.pronouns}</h3>
+          </div>
+          <br/>
+        <div className="dataRow">
+          <small className="dataType">Since | </small>
+          <h3 className="dataCol"> {employee.since}</h3>
+          </div>
+          <br/>
+        <div className="dataRow">
+          <small className="dataType">Email | </small>
+          <h4 className="dataCol"> {employee.email}</h4>
+          </div>
+          <br/>
+        <div className="dataRow">
+          <small className="dataType">ID | </small>
+          <h3 className="dataCol"> {employee.employeeID}</h3>
+          </div>
+          <br/>
+        <div className="dataRow">
+          <small className="dataType">Work Place |</small>
+          <h3 className="dataCol"> {employee.workPlace}</h3>
+          </div>
+
+
+        {/* <h3><small>Role | </small>{employee.role}</h3>
         <h3><small>Pronouns | </small>{employee.pronouns}</h3>
         <h3><small>Since | </small>{employee.since}</h3>
         <h3><small>Email | </small>{employee.email}</h3>
         <h3><small>ID | </small>{employee.employeeID}</h3>
-        <h4>Work Place |{employee.workPlace}</h4>
+        <h4><small>Work Place |</small>{employee.workPlace}</h4> */}
         </div>
         </div>
     </div>
